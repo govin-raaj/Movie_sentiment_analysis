aa05917772c35cf8b1c51a1fab69e847539c86ba
govin-raaj <govin30raaj@gmail.com>
2025-09-10 22:22:32 +0530

    automated the pipeline

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -70,9 +70,9 @@ def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str)
 
 def main():
     try:
-        # params = load_params(params_path='params.yaml')
-        # test_size = params['data_ingestion']['test_size']
-        test_size = 0.2
+        params = load_params(params_path='params.yaml')
+        test_size = params['data_ingestion']['test_size']
+        # test_size = 0.2
         
         df = load_data(data_url='https://raw.githubusercontent.com/govin-raaj/Movie_sentiment_analysis/refs/heads/main/notebooks/data.csv')
         # s3 = s3_connection.s3_operations("bucket-name", "accesskey", "secretkey")
