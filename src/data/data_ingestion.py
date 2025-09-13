3105e7cf3d5c343365163c73f90dcf33370e5026
govin-raaj <govin30raaj@gmail.com>
2025-09-13 16:17:25 +0530

    CICD-3

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -1,6 +1,8 @@
 # data ingestion
 import numpy as np
 import pandas as pd
+
+from src.constants import AWS_ACCESS_KEY, AWS_SECRET_KEY
 pd.set_option('future.no_silent_downcasting', True)
 
 import os
@@ -8,7 +10,7 @@ from sklearn.model_selection import train_test_split
 import yaml
 import logging
 from src.logger import logging
-# from src.connections import s3_connection
+from src.connections import s3_connection
 
 
 def load_params(params_path: str) -> dict:
@@ -75,7 +77,7 @@ def main():
         # test_size = 0.2
         
         df = load_data(data_url='https://raw.githubusercontent.com/govin-raaj/Movie_sentiment_analysis/refs/heads/main/notebooks/data.csv')
-        # s3 = s3_connection.s3_operations("bucket-name", "accesskey", "secretkey")
+        # s3 = s3_connection.s3_operations("moviesentanalydata", AWS_ACCESS_KEY, AWS_SECRET_KEY)
         # df = s3.fetch_file_from_s3("data.csv")
 
 
